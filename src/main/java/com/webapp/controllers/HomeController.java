adaa22dd65a574cd8d0270ae595e2dc15f57ca25
jossuefdez <jofv.01@gmail.com>
2023-08-20 13:34:58 -0600

    Un comentario más.

--- src/main/java/com/webapp/controllers/HomeController.java
+++ src/main/java/com/webapp/controllers/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// Un comentario más.
 @Controller
 @RequestMapping("/")
 public class HomeController {
